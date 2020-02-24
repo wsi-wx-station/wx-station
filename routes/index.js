55f188b1316e187c08e903904f4f2baa5b70e037
Karl Stolley <karl.stolley@gmail.com>
2020-02-24 16:01:49 -0600

    Load up and make use of wx-data module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var wx = require('../lib/wx-data')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WX Station' });
+router.get('/', async function(req, res, next) {
+  let async_data = await wx.returnDummyData();
+  res.render('index', async_data);
 });
 
 module.exports = router;
