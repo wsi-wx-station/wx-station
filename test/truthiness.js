6ddd19d394ad6aebed312a008105d08943e148e2
Karl Stolley <karl.stolley@gmail.com>
2020-02-24 18:11:04 -0600

    Add some silly tests for testing the truth

---
+++ test/truthiness.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('The truth', function() {
+  it('should always be true', function() {
+    assert(true, 'true should always be true');
+  });
+  it('should never be false', function() {
+    assert(!false, 'false shoud never be true')
+  })
+});
