2e83c1f0b9af89874db2b7d962c525f860f5c4e3
Karl Stolley <karl.stolley@gmail.com>
2020-04-06 16:23:05 -0500

    Handle flat-file data from core route and view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,44 @@
 var express = require('express');
+var fs = require('fs').promises;
 var wx = require('../lib/wx-data')
 var router = express.Router();
 
+function shortDate(d) {
+  d = new Date(d);
+  return `${d.getMonth()+1}/${d.getDate()} ${d.getHours()}:${d.getMinutes()}`;
+}
+
+function cardinalDirection(deg) {
+  var cardinals = [
+    'N',
+    'NNE',
+    'NE',
+    'ENE',
+    'E',
+    'ESE',
+    'SE',
+    'SSE',
+    'S',
+    'SSW',
+    'SW',
+    'WSW',
+    'W',
+    'WNW',
+    'NW',
+    'NNW',
+    'N'
+  ];
+  return cardinals[Math.round(deg / 22.5)];
+}
+
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let async_data = await wx.gitHubData();
-  res.render('index', async_data);
+  let wx_json = await fs.readFile('var/wx.json');
+  let wx_data = JSON.parse(wx_json);
+  wx_data.shortDate = shortDate(wx_data.date);
+  wx_data.cardinalDirection = cardinalDirection(wx_data.winddir);
+  res.render('index', wx_data);
 });
 
 module.exports = router;
