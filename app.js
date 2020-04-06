744d0857eef3c2193fe8954bb1dfc2e0f7098656
Karl Stolley <karl.stolley@gmail.com>
2020-04-06 16:07:18 -0500

    Add Ambient Weather API logic

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+// Ambient Weather API
+const AWApi = require('ambient-weather-api');
+
+const apiKey = process.env.AMBIENT_WEATHER_USER_KEY;
+
+const api = new AWApi({
+  apiKey: apiKey,
+  applicationKey: process.env.AMBIENT_WEATHER_APP_KEY
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -44,5 +54,23 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+api.connect();
+api.on('connect', function() {
+  console.log('Connected to the Ambient Weather API');
+});
+
+api.on('subscribed', function(ddata) {
+  // console.log(data.devices[0].lastData);
+  var data = ddata.devices[0].lastData;
+  console.log(`At ${shortDate(data.date)} - ${data.tempf}℉, wind out of the ${cardinalDirection(data.winddir)} at ${data.windspeedmph} mph`);
+});
+
+api.on('data', function(data){
+  console.log(`At ${shortDate(data.date)} - ${data.tempf}℉, wind out of the ${cardinalDirection(data.winddir)} at ${data.windspeedmph} mph`);
+});
+
+api.subscribe(apiKey);
+
 
 module.exports = app;
