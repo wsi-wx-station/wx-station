06c92f07a27bfef0f92afe69eb7be3046628bc5a
Karl Stolley <karl.stolley@gmail.com>
2020-04-14 15:27:41 -0500

    Add UN index and risk to initial data

--- routes/index.js
+++ routes/index.js
@@ -31,11 +31,33 @@ function cardinalDirection(deg) {
   return cardinals[Math.round(deg / 22.5)];
 }
 
+function uvIndex(uv) {
+  var risk;
+  switch(uv) {
+    case (uv > 10):
+      risk = "Extreme";
+      break;
+    case (uv > 7):
+      risk = "Very High";
+      break;
+    case (uv > 5):
+      risk = "High";
+      break;
+    case (uv > 2):
+      risk = "Moderate";
+      break;
+    default:
+      risk = "Low";
+  }
+  return { index: uv, risk: risk, class: risk[0].toLowerCase() };
+}
+
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   let wx_json = await fs.readFile('var/wx.json');
   let wx_data = JSON.parse(wx_json);
+  wx_data.uv = uvIndex(wx_data.uv);
   wx_data.shortDate = shortDate(wx_data.date);
   wx_data.cardinalDirection = cardinalDirection(wx_data.winddir);
   res.render('index', wx_data);
