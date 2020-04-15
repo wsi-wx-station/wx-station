5c25b40458ff903f569f02916c8d1edba0be30a0
Karl Stolley <karl.stolley@gmail.com>
2020-04-15 14:34:24 -0500

    Silence all linter errors

--- test/wx-data-test.js
+++ test/wx-data-test.js
@@ -1,5 +1,8 @@
-const assert = require('assert').strict;
+/* eslint-env mocha */
+/* eslint no-sync: "off" */
+'use strict';
 
+const assert = require('assert').strict;
 const wx = require('../lib/wx-data');
 
 /* Testing a run-of-the-mill synchronous function */
