b572b8863e6bd1ddf6cd2f69198b7d44242368ba
Karl Stolley <karl.stolley@gmail.com>
2020-04-15 14:17:28 -0500

    Correct syntax error in shortDate()

--- public/js/site.js
+++ public/js/site.js
@@ -62,7 +62,7 @@ function zeroPad(num,length) {
 function shortDate(d) {
   d = new Date(d);
   // return `${d.getMonth()+1}/${d.getDate()} ${d.getHours()}:${zeroPad(d.getMinutes())}`;
-  return (d.getMonth() + 1) + '/' + d.getDate() + ' ' d.getHours() + ':' + zeroPad(d.getMinutes());
+  return (d.getMonth() + 1) + '/' + d.getDate() + ' ' + d.getHours() + ':' + zeroPad(d.getMinutes());
 }
 
 function cardinalDirection(deg) {
