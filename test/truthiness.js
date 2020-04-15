f18d03fd11fdb2d1f92a5cd4c916a6ae9996df23
Karl Stolley <karl.stolley@gmail.com>
2020-04-15 13:15:33 -0500

    Add missing semicolons

--- test/truthiness.js
+++ test/truthiness.js
@@ -6,6 +6,6 @@ describe('The truth', function() {
     assert(true, 'true should always be true');
   });
   it('should never be false', function() {
-    assert(!false, 'false shoud never be true')
-  })
+    assert(!false, 'false shoud never be true');
+  });
 });
