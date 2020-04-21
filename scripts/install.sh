2d8574fdb1bf1f6338d6a091a9c0ba5d4f4defe1
Karl Stolley <karl.stolley@gmail.com>
2020-04-20 22:45:19 -0500

    Update install script to be run from app dir

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,2 @@
-ln -s ../../scripts/pre-commit.sh ../.git/hooks/pre-commit
-git update-index --skip-worktree var/wx.json
+ln -s scripts/pre-commit.sh .git/hooks/pre-commit
+# git update-index --skip-worktree var/wx.json
